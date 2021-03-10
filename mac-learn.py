bd27ca037b2c30c55054be604e66867f85eaf504
rodartes <rodartes@oregonstate.edu>
2021-03-09 18:53:51 -0800

    load csv using numpy (last was python standard)

--- mac-learn.py
+++ mac-learn.py
@@ -1,12 +1,5 @@
-import csv
-import numpy as np 
-path = r"iris.csv"
-with open(path, 'r') as f:
-    reader = csv.reader(f, delimiter = ',')
-    headers = next(reader)
-    data = list(reader)
-    data = np.array(data)
-
-    print(headers)
-    print(data.shape)
-    print(data[:3])
+from numpy import loadtxt
+datapath = open("diabetes.csv", 'r')
+data = loadtxt(datapath, delimiter=",", skiprows=1)
+print(data.shape)
+print(data[:3])
